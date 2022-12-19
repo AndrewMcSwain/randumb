172c7b43bad07c9d69cdb79e021f5c7e9d0d104f
Andrew Edwin McSwain <andrewmcswain@gmail.com>
2022-12-19 18:10:40 -0300

    building functionality dynamic key assignment from array index. WIP. currently: conditional hash creation

--- data_model_prototype.rb
+++ data_model_prototype.rb
@@ -3,37 +3,56 @@
 #data structure mock-up                              #
 
 # abstract into function the index_to_s 
-# 
-list_of_fn = [ x, y, z]
+#
+list_of_fn = [ 1, 2, 3]
+
+class DataModel
+  def arr_i_to_hash_k(arr, hash=nil)
+    if defined?(hash) == false
+      hash = Hash.new
+    else
+      arr.each_with_index do |element, index|
+        hash.store(index, element)
+      end
+    end
+    return hash
+  end
+end
+
+
+  aresult = DataModel.new  
+  aresult.arr_i_to_hash_k(list_of_fn) 
+  puts aresult
 # actual: 
-  result[:fn"#{index_to_s(list_of_fn)}"]
-          [:v"#{index_to_s(result[fn..]
-                                 [index_of_v]
-                          )
-               }"
-        ]
+#  result[":fn#{fn_list[i]}"]
+        #make this correct and comprehisible  
+#          [:v"#{index_to_s(result[fn..]
+#                                 [index_of_v]
+#                          )
+#               }"
+#        ]
+#
+#
+#    result = { fn1 => [ v1 => mdarr,
+#                        v2 => mdarr,
+#                       v3 => mdarr,
+#                        ...
+#               ],
+#               fn2 => [ v1 => mdarr,
+#                        v2 => mdarr,
+#                        v3 => mdarr,
+#                        ...
+#               ],
+#               fn3 => [ v1 => mdarr,
+#                        v2 => mdarr,
+#                        v3 => mdarr.
+#                        ...
+#               ],
+#               ...
+#              }
 
 
-    result = { fn1 => [ v1 => mdarr,
-                        v2 => mdarr,
-                        v3 => mdarr,
-                        ...
-               ],
-               fn2 => [ v1 => mdarr,
-                        v2 => mdarr,
-                        v3 => mdarr,
-                        ...
-               ],
-               fn3 => [ v1 => mdarr,
-                        v2 => mdarr,
-                        v3 => mdarr.
-                        ...
-               ],
-               ...
-              }
-
-
-     #refactor mdarr, cogitar seperating derivative analysis from calculation result
+     #refactor mdarr, cogitate seperating derivative analysis from calculation result
     # represent in this mockup that algo will be run many times with different inputs
         
     #mdarr
